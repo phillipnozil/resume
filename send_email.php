d9cb4ae7dbe20376e213ffc4e9a40a335f396743
phillipnozil <Phillipnozil@gmail.com>
2024-11-14 02:18:31 -0500

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST['message']);
     $to = "Phillipnozil@gmail.com";
     $subject = "New Message from Website";
-    $headers = "From: no-reply@gmail.com";
+    $headers = "From: ";
 
     if (mail($to, $subject, $message, $headers)) {
         echo "Message sent successfully!";
